65d815ef550543f2db5dbec543a1b5d01b95e378
RakMykola <natonrak@gmail.com>
2023-06-29 12:28:20 +0300

    les1

--- les1/les1.js
+++ les1/les1.js
@@ -1,1 +1,41 @@
-console.log("kdjkjd");
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let i = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let e = 3.14;
+let w = true;
+let r = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(i);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(e);
+console.log(w);
+console.log(r);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//  З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+const firstName = "Rak";
+const middleName = "Mykola";
+console.log(firstName + ' ' + middleName);
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let ab = 100;
+let bc = '100';
+let cd = true;
+
+console.log(typeof ab, typeof bc, typeof cd);
+
